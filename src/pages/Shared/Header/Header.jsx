58d8a9458578b0a3a9418d0f95235e554d3da5dd
mirazfaruk <mirazfaruk@gmail.com>
2024-08-21 22:57:32 +0600

    Added header

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,7 +1,12 @@
+import logo from '../../../assets/logo.png';
+import moment from 'moment';
+
 const Header = () => {
     return (
-        <div>
-            <h3 className="text-3xl">Header</h3>
+        <div className='flex flex-col items-center justify-center'>
+            <img className='pb-5' src={logo} alt="" />
+            <p className='text-lg text-[#706F6F] pb-[10px]'>Journalism Without Fear or Favour</p>
+            <p className='text-xl font-medium'><span className='text-[#403F3F]'>{moment().format('dddd,')}</span><span className='text-[#706F6F]'>{moment().format(' MMMM D, YYYY')}</span></p>
         </div>
     );
 };
