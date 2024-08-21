8dc2f3f7621d9f9cf81da7e9f212a5624cef0208
mirazfaruk <mirazfaruk@gmail.com>
2024-08-21 23:37:55 +0600

    Added navbar

--- src/pages/Home/BreakingNews.jsx
+++ src/pages/Home/BreakingNews.jsx
@@ -3,7 +3,7 @@ import Marquee from "react-fast-marquee";
 
 const BreakingNews = () => {
     return (
-        <div className="flex p-4 pr-24 bg-[#F3F3F3]">
+        <div className="flex p-4 pr-24 bg-[#F3F3F3] mb-5">
             <button className="bg-[#D72050] text-white px-6 py-2 text-xl font-medium mr-5">Latest</button>
             <Marquee pauseOnHover={true} speed={100} className="text-[#403F3F] font-semibold">
                 <Link className="mr-10">Match Highlights: Germany vs Spain — as it happened   !   Match Highlights: Germany vs Spain as...</Link>
