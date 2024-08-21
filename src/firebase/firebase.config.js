e7a5dd42625c79a0667c3cd485782288444573fb
mirazfaruk <mirazfaruk@gmail.com>
2024-08-21 22:30:20 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCrLW7vgOmzU1ff1Y8DS0zUUE0wKUuJOvI",
+    authDomain: "react-dragon-news-auth-1d852.firebaseapp.com",
+    projectId: "react-dragon-news-auth-1d852",
+    storageBucket: "react-dragon-news-auth-1d852.appspot.com",
+    messagingSenderId: "831532996233",
+    appId: "1:831532996233:web:53d9370f3aa604ef409858"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
---
+++ src/pages/Shared/RightSideNav/RightSideNav.jsx
@@ -0,0 +1,9 @@
+const RightSideNav = () => {
+    return (
+        <div>
+            <h2 className='text-2xl'>Right side</h2>
+        </div>
+    );
+};
+
+export default RightSideNav;
